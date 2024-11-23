2685521a2ced1dd603dad8252b40b31d54c7d8e7
Oumaima0762 <ouma.hajj123@gmail.com>
2024-11-23 21:59:18 +0100

    front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MenuAdminComponent } from './Admin/menu-admin/menu-admin.component';
 import { DashboardComponent } from './Admin/dashboard/dashboard.component';
@@ -10,6 +10,8 @@ import { GroupesComponent } from './Admin/groupes/groupes.component';
 import { MatieresComponent } from './Admin/matieres/matieres.component';
 import { NiveauxComponent } from './Admin/niveaux/niveaux.component';
 import { EtablissementsComponent } from './Admin/etablissements/etablissements.component';
+import { CoursComponent } from './Admin/cours/cours.component';
+
 
 
 @NgModule({
@@ -22,11 +24,14 @@ import { EtablissementsComponent } from './Admin/etablissements/etablissements.c
     MatieresComponent,
     NiveauxComponent,
     EtablissementsComponent,
+    CoursComponent
+    
     
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+     FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
