89ad92e2585e6668ec083451ab2ca94cae419515
Oumaima0762 <ouma.hajj123@gmail.com>
2024-12-21 09:24:26 +0100

    Gs

--- src/app/Admin/gestion-scolaire/gestion-scolaire.component.ts
+++ src/app/Admin/gestion-scolaire/gestion-scolaire.component.ts
@@ -12,5 +12,37 @@ export class GestionScolaireComponent {
 
   constructor(private _service: GlobalService) {}
   
+ 
 
+   formsData = {
+    typeFrais: '',
+    montant: null,
+    description: '',
+    annee:'',
+  };
+
+  
+  // Define the editData object with default values
+  editData = {
+    annee: '',
+    typeFrais: '',
+    montant: null,
+    description: ''
+  };
+
+  // Add the onSubmitEdit method
+  onSubmitEdit(form: any) {
+    if (form.valid) {
+      console.log('Form Data:', this.editData);
+      // Implement your logic here
+    }
+  }
+
+  openEditModal(itemId: number) {
+  console.log("Edit modal opened for item ID:", itemId);
+  // Add your logic here
+}
+
+  
+ 
 }
